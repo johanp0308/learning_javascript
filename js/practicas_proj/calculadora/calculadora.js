ac66732356d8802eac3a9a6525438049c619d8fb
johanp0308 <sebastian-johan1@hotmail.com>
2023-09-08 13:20:22 -0500

    commiteo de salvado

--- js/practicas_proj/calculadora/calculadora.js
+++ js/practicas_proj/calculadora/calculadora.js
@@ -2,14 +2,15 @@ class Calculadora{
     sumar(num1, num2){
         return num1 + num2;
     }
-
     restar(num1, num2){
-        return num1 + num2;
+        return num1 - num2;
     }
-    dviidir(num1, num2){
-        return num1 + num2;
+    dividir(num1, num2){
+        return num1 / num2;
     }
     multiplicar(num1, num2){
-        return num1 + num2;
+        return num1 * num2;
     }
+
+    
 }
